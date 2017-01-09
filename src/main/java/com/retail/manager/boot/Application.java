4dbbd62da7aa62727f29b0944a7a1a01b3a95f94
sugandhamahajan <mahajan.sugandha@gmail.com>
2017-01-09 21:44:34 +0530

    Add files via upload

--- src/main/java/com/retail/manager/boot/Application.java
+++ src/main/java/com/retail/manager/boot/Application.java
@@ -1,1 +1,35 @@
-https://github.com/sugandhamahajan/Retail_Management/new/development
+/**
+ * 
+ */
+package com.retail.manager.boot;
+
+/**
+ * @author sugandha
+ *
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
+
+import com.retail.manager.configuration.ApplicationConfiguration;
+
+@SpringBootApplication
+@Import({ApplicationConfiguration.class})
+public class Application extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(Application.class);
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
