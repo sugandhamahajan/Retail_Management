5b076a90885631b3935aaf32bba213de100728e0
sugandhamahajan <mahajan.sugandha@gmail.com>
2017-01-09 22:24:24 +0530

    Add files via upload

---
+++ src/main/java/com/retail/manager/domain/ShopAddress.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.retail.manager.domain;
+
+/**
+ * @author sugandha
+ *
+ */
+public class ShopAddress {
+	
+	private String number;
+	private String postCode;
+	
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getPostCode() {
+		return postCode;
+	}
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+	
+
+}
